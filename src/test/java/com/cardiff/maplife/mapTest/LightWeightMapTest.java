cbbbcf38847ee68c081e01735e67ec59f5fe36ba
Yanguo Cao <caoy35@cardiff.ac.uk>
2022-10-18 00:49:38 +0100

    Finish  lightweight components test eventService and map page

--- src/test/java/com/cardiff/maplife/mapTest/LightWeightMapTest.java
+++ src/test/java/com/cardiff/maplife/mapTest/LightWeightMapTest.java
@@ -1,12 +1,22 @@
 package com.cardiff.maplife.mapTest;
 
 import com.cardiff.maplife.controllers.MainController;
+import com.cardiff.maplife.entities.Event;
+import com.cardiff.maplife.services.EventService;
+import com.cardiff.maplife.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -18,6 +28,10 @@ import static org.hamcrest.Matchers.containsString;
 public class LightWeightMapTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private EventService eventService;
 
     // Simple Unit tests using a mockMVC - no change here from container testing as no access to database
     @Test
@@ -25,4 +39,18 @@ public class LightWeightMapTest {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Clear All")));
     }
+    //  Component test using a mockMVC but mocking the repository response (Note the @MockBean)
+    //  This only tests the controller and the view layers
+    @Test
+    public void isEventOnMapTest() throws Exception {
+        Date eventDate = new Date();
+        Timestamp datetime = new Timestamp(System.currentTimeMillis());
+        Event event = new Event(1L,1L,"test event",0,0,false,eventDate,"test event dis",true);
+        // This will return what the service would have returned - a list of events called method in the repo
+        given(this.eventService.findCustom(datetime)).willReturn(List.of(event));
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("test event")));
+    }
+
+
 }
